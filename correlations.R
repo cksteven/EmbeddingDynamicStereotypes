a046a741531d2e4e395c5e5019dc06b15a9fae6f
Kesong Cao <kesong.cao@wisc.edu>
2022-05-11 14:02:06 -0500

    some resultsgit add .git add .

--- correlations.R
+++ correlations.R
@@ -15,6 +15,30 @@ d1 = d1[d1$year < 2010, ]
 filtered_occups = c('accountant', 'architect', 'artist', 'athlete', 'attendant','author', 'baker', 'broker', 'carpenter', 'cashier', 'clergy','clerical', 'clerk', 'conductor', 'cook', 'dancer', 'dentist','designer', 'driver', 'economist', 'engineer', 'farmer','gardener', 'housekeeper', 'laborer', 'librarian', 'mathematician','midwife', 'musician', 'nurse', 'official', 'operator','photographer', 'pilot', 'police', 'porter', 'professor','psychologist', 'retired', 'sailor', 'scientist', 'secretary','soldier', 'student', 'tailor', 'teacher')
 d = d2[,c("year",filtered_occups)]
 
+lag_df = lag_df[(lag_df$max_lag >= -5) & (lag_df$max_lag <= 5),]
+
+mean(lag_df$max_lag)
+mean(lag_df$max_acf)
+
+d3 = read.csv("IPUMS.csv") %>% janitor::clean_names()
+d3 = d3[d3$sex == 1,] %>% select(-sex, -x) %>% rename(occup = single_words)
+d3 = d3[d3$occup %in% filtered_occups,]
+
+occup_stats = d3 %>% pivot_wider(
+        id_cols = year,
+        names_from = occup,
+        values_from = percent)
+
+write.csv(occup_stats, "occup_stats.csv")
+
+occup_stats = occup_stats[occup_stats$year %in% d$year,]
+occup_na = c("dancer", "dentist", "designer", "gardener", "musician", "porter", "professor", "retired")
+occup_stats = occup_stats %>% select(!all_of(occup_na))
+occup_stats$occup_m = rowMeans(occup_stats[,2:39], na.rm = T)
+
+d = d %>% select(!all_of(occup_na))
+d$occup_m = rowMeans(d2[,2:39])
+
 d$occup_m = rowMeans(d2[,2:47])
 d$gss_m = d1$attitude
 
@@ -34,18 +58,20 @@ ggplot(data=d, aes(x=year)) +
 
 res = ccf(d$occup_m, d$gss_m, lag = 10)
 
-ccf(d$teacher, d2$gss_m, lag = 10)
-ccf(d$soldier, d2$gss_m, lag = 10)
+#ccf(d$teacher, d2$gss_m, lag = 10)
+#ccf(d$soldier, d2$gss_m, lag = 10)
 
-ccf(d$police, d2$gss_m, lag = 10)
-ccf(d$doctor, d2$gss_m, lag = 10)
+#ccf(d$police, d2$gss_m, lag = 10)
+#ccf(d$doctor, d2$gss_m, lag = 10)
 
-ccf(d$dancer, d2$gss_m, lag = 10)
-ccf(d$engineer, d2$gss_m, lag = 10)
-ccf(d$nurse, d2$gss_m, lag = 10)
+#ccf(d$dancer, d2$gss_m, lag = 10)
+#ccf(d$engineer, d2$gss_m, lag = 10)
+#ccf(d$nurse, d2$gss_m, lag = 10)
 
 max_lags = c()
 max_acfs = c()
+filtered_occups = filtered_occups[!filtered_occups %in% occup_na]
+
 for (occup in filtered_occups){
   res = ccf(d[,occup], d$gss_m, lag = 10, plot=F)
   res_df = do.call(rbind, Map(data.frame, lag=res$lag, acf=res$acf))
@@ -63,18 +89,8 @@ mean(max_acfs)
 lag_df = do.call(rbind, Map(data.frame, occup=filtered_occups, max_lag=max_lags, max_acf=max_acfs))
 write.csv(lag_df, "lag_df.csv")
 
-lag_df = lag_df[(lag_df$max_lag >= -5) & (lag_df$max_lag <= 5),]
 
-mean(lag_df$max_lag)
-mean(lag_df$max_acf)
 
-d3 = read.csv("IPUMS.csv") %>% janitor::clean_names()
-d3 = d3[d3$sex == 1,] %>% select(-sex, -x) %>% rename(occup = single_words)
-d3 = d3[d3$occup %in% filtered_occups,]
 
-occup_stats = d3 %>% pivot_wider(
-        id_cols = year,
-        names_from = occup,
-        values_from = percent)
 
-write.csv(occup_stats, "occup_stats.csv")
+
